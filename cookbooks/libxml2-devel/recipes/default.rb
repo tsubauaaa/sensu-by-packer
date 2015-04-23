239afeac9fb446c64229436c7b3147cc03515852
tsubauaaa <tsubasa11@marble.ocn.ne.jp>
2015-04-24 00:58:01 +0900

    libxml2-devel cookbook add

---
+++ cookbooks/libxml2-devel/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: libxml2-devel
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "libxml2-devel" do
+  action [:install, :upgrade]
+end
