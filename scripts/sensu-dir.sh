bc621558210ff1b8697db988ee3e0ac74dcbedd0
tsubauaaa <tsubasa11@marble.ocn.ne.jp>
2015-04-24 00:57:39 +0900

    install aws-sdk-v1

--- scripts/sensu-dir.sh
+++ scripts/sensu-dir.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+export PATH=$PATH:/opt/sensu/embedded/bin/
+
+gem install aws-sdk-v1
+
 chmod -R 755 /etc/sensu/plugins
 chmod -R 744 /etc/sensu/conf.d/checks
-chown -R root:sensu /etc/sensu/plugins
-chown -R root:sensu /etc/sensu/conf.d/checks
+chown -R sensu:sensu /etc/sensu/plugins
+chown -R sensu:sensu /etc/sensu/conf.d/checks
